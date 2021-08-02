6c4329cf31e7e125787e38f8a5c35625daf70783
sven <sven@so36.net>
2021-08-02 13:52:18 +0200

    docs(guides) wip add guidelines

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -44,6 +44,8 @@ module.exports = {
       children: [
         'overview/resources',
         'overview/use-cases',
+        'overview/design-principles',
+        'overview/developer-good-practices'
 
       ]
     },
@@ -80,6 +82,7 @@ module.exports = {
           'frontend/appextras',
           'frontend/contextnavigation',
           'frontend/pluggables',
+          'frontend/design',
         ]
       },
       {
